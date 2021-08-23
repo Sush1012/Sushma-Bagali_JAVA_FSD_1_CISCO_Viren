b2359c5a75dc067f6cb57a136ef1a2f509fddaee
Sushma Bagali <53126922+Sush1012@users.noreply.github.com>
2021-08-23 22:57:10 +0530

    Day3 Assignment (19/08/2021)

---
+++ Company/Infosys_Company.java
@@ -0,0 +1,10 @@
+package Company;
+
+public abstract class Infosys_Company extends MNC_Company{
+	
+
+	void show() {
+		System.out.println("Implemented show method of Parent Class");
+	}
+	abstract void disp();
+}
